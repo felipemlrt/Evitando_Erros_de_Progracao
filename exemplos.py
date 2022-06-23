fc52a91f9e9b3311333ef8197aab5d0d1d21d9f7
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2022-06-22 21:07:51 -0300

    Create exemplos.py

---
+++ exemplos.py
@@ -0,0 +1,29 @@
+#
+x = int(1.29)
+print (x)
+
+#
+teste = ["primeiro_valor", "segundo_valor"]
+print (teste[0])
+
+#
+x = input("Digite um inteiro: ")
+if x == 5:
+     print("x é igual a 5")
+if x == "5":
+     print("x é igual a '5' ")
+
+#
+x = int( input("Digite um inteiro: ") )
+if x == 5:
+     print("x é igual a 5")
+     
+#
+for i in range(0, 10):
+     x = 0
+     print(x)
+     x + 1
+    
+#
+print ( 10+5/3*2)
+print ( (10+5)/(3*2) )
